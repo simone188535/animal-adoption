eff7901bfc42ec651e7e49893a47ece5375bb19e
Simone Anthony <simoneanthony@Simones-MBP.lan>
2021-08-07 22:04:17 -0400

    styling edits

--- src/components/HomepageHeader/index.tsx
+++ src/components/HomepageHeader/index.tsx
@@ -5,10 +5,13 @@ const HomepageHeader: React.FC = () => {
   return (
     <div
       style={{ backgroundImage: "url(./images/adoption.jpg)" }}
-      className="container-fluid homepage-header"
+      className="container-fluid homepage-header d-flex "
     >
+      <div className="transparent-cover h-100 w-100 d-flex align-items-center justify-content-center flex-column">
       {/* <img src="./images/adoption.jpg"/> */}
-      Header test
+      <h1 className="text-light ">Animal Adoption</h1>
+      <p className="text-light"><strong>*Disclaimer: </strong> This is just a test project and is not actually active</p>
+      </div>
     </div>
   );
 };
