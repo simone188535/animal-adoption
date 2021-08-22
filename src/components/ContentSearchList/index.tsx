8879c23a4d762a1bcf4fe8ff0382342b0a6c06d7
Simone Anthony <simoneanthony@Simones-MBP.lan>
2021-08-21 23:06:39 -0400

    comments added button logic init

--- src/components/ContentSearchList/index.tsx
+++ src/components/ContentSearchList/index.tsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./contentsearchlist.scss";
 
 const ContentSearchList: React.FC = () => {
+  const [activeButton, setActiveButton] = useState(0);
+  const [searchResult, setSearchResult] = useState([]);
+
+  // const searchButtonOptions = ["Dogs", "Cats"];
+  const toggleSearch = () => {
+    // If activeButton equals the map index, add active class
+    console.log('Hello World');
+  }
+
   return (
     <div className="container content-search-list-container">
       <div className="d-flex justify-content-center w-100 my-5">
         <h4 className="content-search-list-header">Please Select Your Preferred Animal</h4>
       </div>
       <div className="d-flex justify-content-center">
-      <button type="button" className="btn btn-warning w-25 mx-3">Dogs</button>
-      <button type="button" className="btn btn-warning w-25 mx-3">Cats</button>
+        {/* put values using array and map them */}
+      <button type="button" className="btn btn-warning w-25 mx-3" onClick={() => toggleSearch}>Dogs</button>
+      <button type="button" className="btn btn-warning w-25 mx-3" onClick={() => toggleSearch}>Cats</button>
       </div>
     </div>
   );
